eb420a94a775d294ad40fdaf658533ab087e5c18
Aya ElAkhras <ayaelakhras@aucegypt.edu>
2020-08-05 19:02:45 +0200

    Update raptor_UART.cs

--- raptor_UART.cs
+++ raptor_UART.cs
@@ -20,59 +20,101 @@ using Antmicro.Renode.Core;
 using Antmicro.Renode.Logging;
 using Antmicro.Renode.Peripherals.Bus;
 using System.Collections.Generic;
-using Antmicro.Migrant;
 using Antmicro.Renode.Core.Structure.Registers;
 
 namespace Antmicro.Renode.Peripherals.UART
 {
-	public class Raptor_UART : UARTBase
+	public class Raptor_UART : UARTBase, IDoubleWordPeripheral, IKnownSize
 	{
 		public Raptor_UART(Machine machine, uint frequency = 50000000) : base(machine)
 		{
 			this.frequency = frequency;
-			//DefineRegisters();
-		}
+						
+			var registersMap = new Dictionary<long, DoubleWordRegister>
+		        {
+				{(long)Registers.TransmitData, new DoubleWordRegister(this)
+				    .WithValueField(0, 8, FieldMode.Write, writeCallback: (_, b) =>
+				    {
+					if(transmitEnable.Value) // Register field providing a boolean value declared below
+					{
+					    this.TransmitCharacter((byte)b);    // field in the UARTBase class
+					    UpdateInterrupts();
+					}
+					else
+					{
+					    this.Log(LogLevel.Warning, "Trying to transmit '{1}' (0x{0}), but the transmitter is disabled", b, (char) b);
+					}
+				    }, name: "DATA")
+				    .WithTag("RESERVED", 8, 23)
+				    .WithFlag(31, valueProviderCallback: _ => false, name: "FULL")
+				},
 
-		public override void WriteChar(byte value)
-        	{
-			if(receiveFifo.Count == 10)  // fifo is full, so block until empty
-			{
-					// todo: block execution until a place is emptied
-			}
+				{(long)Registers.ReceiveData, new DoubleWordRegister(this)
+				    // the "EMPTY" flag MUST be declared before "DATA" value field because 'Count' value
+				    // might change as a result of dequeuing a character; otherwise if the queue was of
+				    // length 1, the read of this register would return both the character and "EMPTY" flag
+				    .WithFlag(31, valueProviderCallback: _ => Count == 0, name: "EMPTY")   // Count gets updated with the Queue size in UARTBase
+				    .WithValueField(0, 8, FieldMode.Read, valueProviderCallback: _ =>
+				    {
+					if(!TryGetCharacter(out var character))  // function in the UARTBase that returns False if the Queue is empty
+					{
+					    this.Log(LogLevel.Warning, "Trying to read data from empty receive fifo");
+					}
+					return character;
+				    }, name: "DATA")
+				    .WithTag("RESERVED", 8, 23)
+				 },
+			};			
+			
+			registers = new DoubleWordRegisterCollection(this, registersMap);
+			IRQ = new GPIO();   // todo: double check the interrupts
 
-			receiveFifo.Enqueue(value);
-			// todo: raise an interrupt
-
-        	}
-
-		public byte ReadChar()
+			Reset();
+			
+			
+		}   // end of constructor
+		
+		public uint ReadDoubleWord(long offset)   
 		{
-			if(sendFifo.Count == 0)
-			{
-				// todo: block execution until a it becomes not empty
+			lock(innerLock)
+			{	
+				return registers.Read(offset);
 			}
-			return sendFifo.Dequeue();
 		}
 
+		public void WriteDoubleWord(long offset, uint value)
+		{
+			lock(innerLock)
+			{
+				registers.Write(offset, value);
+			}
+		}
+		
 		public override void Reset()   
 		{
 		    base.Reset();
-		    receiveFifo.Clear();
-		    sendFifo.Clear();
-		}
-
-		// todo
-		// Process read and write with interrupt
-
-		private enum Registers   // Need to check the addresses 
-		{
-		    BaudRate = 0x10,
-		    Status = 0x14,
-		    Control = 0x18,
-		    Data = 0x1c,
-		    Fifo = 0x28
+		    
+		    registers.Reset();
+		    UpdateInterrupts();
+		   
 		}
 		
+		
+		private void UpdateInterrupts()
+		{
+			lock(innerLock)
+			{
+				transmitWatermarkInterruptPending.Value = (transmitWatermarkInterruptEnable.Value && transmitWatermarkLevel.Value > 0);
+				receiveWatermarkInterruptPending.Value = (receiveWatermarkInterruptEnable.Value && Count > receiveWatermarkLevel.Value);
+
+				IRQ.Set(transmitWatermarkInterruptPending.Value || receiveWatermarkInterruptPending.Value);
+			}
+		}
+
+		private readonly uint frequency;  // supposing this is the sysclk
+		
+		private readonly IFlagRegisterField transmitEnable;
+		private readonly IFlagRegisterField receiveEnable;
 
 		public override Parity ParityBit
 		{
@@ -99,12 +141,14 @@ namespace Antmicro.Renode.Peripherals.UART
 				return frequency / 163*16;
 			}
 		}
-
-		private readonly uint frequency;  // supposing this is the sysclk
 		
-		// 2 fifos for sending and receiving data
-		private readonly Queue<byte> receiveFifo = new Queue<byte>();
-		private Queue<byte> sendFifo = new Queue<byte>();
+		private enum Registers : long   // Need to check the addresses 
+		{
+			TransmitData = 0x0,
+			ReceiveData = 0x04
+			// CLK_DIV = 0x08   // todo
+		}
+		
 
 	}
 
